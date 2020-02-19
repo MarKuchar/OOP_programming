c698dd483edc6c49e7372d0ef1208c92d47524b4
Martin Kuchar <martin.kuchar@Martins-MacBook-Pro.local>
2020-02-19 09:26:59 -0800

    Add Lab1

---
+++ src/practice/Rectangle.java
@@ -0,0 +1,59 @@
+package practice;
+
+class Rectangle {
+    private int height;
+    private int width;
+    private String color;
+
+    public Rectangle(int height, int width, String color) {
+    setHeight(height);
+    setWidth(width);
+    setColor(color);
+    }
+
+    public void setHeight(int h) {
+        if (h <= 1) {
+            this.height = 1;
+        }
+        else {
+            this.height = h;
+        }
+    }
+    public void setWidth(int w) {
+        if (w <= 1) {
+            this.width = 1;
+        }
+        else {
+            this.width = w;
+        }
+    }
+    public void setColor(String color) {
+        if (Character.isUpperCase(color.charAt(0)) && color.length() < 20 ) {
+            this.color = color;
+        }
+        else {
+            this.color = "Blue";
+        }
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getWidth() {
+        return width;
+    }
+
+    public void draw() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(color.charAt(0));
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
+
